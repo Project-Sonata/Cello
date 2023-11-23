bd66309fd5ab252184fb1e46c6ee8855d55944b4
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-23 12:55:14 +0200

    Marker interface to indicate the instance about client info

---
+++ src/main/java/com/odeyalo/sonata/cello/core/client/Oauth2ClientInfo.java
@@ -0,0 +1,11 @@
+package com.odeyalo.sonata.cello.core.client;
+
+/**
+ * Additional info about the Oauth2 Client, default implementation is {@link EmptyOauth2ClientInfo}
+ * <p>
+ * Used as marker interface and can be cast to more detailed instance.
+ * <p>
+ * Implementations can contain any info, such client name, logo, etc
+ */
+public interface Oauth2ClientInfo {
+}

9730927f4faa2e22a05948affeb1eea9f94c0740
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-23 17:09:10 +0200

    Added hardcoded values

--- src/main/java/com/odeyalo/sonata/cello/web/Oauth2Controller.java
+++ src/main/java/com/odeyalo/sonata/cello/web/Oauth2Controller.java
@@ -3,6 +3,7 @@ package com.odeyalo.sonata.cello.web;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -14,7 +15,11 @@ import static org.springframework.http.MediaType.TEXT_HTML;
 public class Oauth2Controller {
 
     @GetMapping(value = "/authorize")
-    public Mono<ResponseEntity<?>> handleAuthorize() {
+    public Mono<ResponseEntity<?>> handleAuthorize(@RequestParam("response_type") String responseType,
+                                                   @RequestParam("client_id") String clientId,
+                                                   @RequestParam("redirect_uri") String redirectUri,
+                                                   @RequestParam("scope") String scope,
+                                                   @RequestParam("state") String state) {
         return Mono.just(ResponseEntity
                 .status(OK)
                 .contentType(TEXT_HTML)

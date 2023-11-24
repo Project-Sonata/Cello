6b601ec900eafc6f6eeb401c51755b2d42d00013
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-24 12:59:12 +0200

    Exception to throw when AuthorizationRequest cannot be created

---
+++ src/main/java/com/odeyalo/sonata/cello/exception/AuthorizationRequestCreationException.java
@@ -0,0 +1,37 @@
+package com.odeyalo.sonata.cello.exception;
+
+import com.odeyalo.sonata.cello.core.AuthorizationRequest;
+
+/**
+ * Exception to throw when {@link AuthorizationRequest} cannot be created
+ */
+public class AuthorizationRequestCreationException extends RuntimeException {
+    
+    public static AuthorizationRequestCreationException defaultException() {
+        return new AuthorizationRequestCreationException();
+    }
+
+    public static AuthorizationRequestCreationException withCustomMessage(String message) {
+        return new AuthorizationRequestCreationException(message);
+    }
+
+    public static AuthorizationRequestCreationException withMessageAndCause(String message, Throwable cause) {
+        return new AuthorizationRequestCreationException(message, cause);
+    }
+
+    public AuthorizationRequestCreationException() {
+        super();
+    }
+
+    public AuthorizationRequestCreationException(String message) {
+        super(message);
+    }
+
+    public AuthorizationRequestCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthorizationRequestCreationException(Throwable cause) {
+        super(cause);
+    }
+}

99a974a4f8b9b5242db79dd1a86d7842a512c7d7
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-25 18:51:01 +0200

    Returns hardcoded html page now

--- src/main/java/com/odeyalo/sonata/cello/web/Oauth2Controller.java
+++ src/main/java/com/odeyalo/sonata/cello/web/Oauth2Controller.java
@@ -1,25 +1,34 @@
 package com.odeyalo.sonata.cello.web;
 
 import com.odeyalo.sonata.cello.core.AuthorizationRequest;
+import org.javaync.io.AsyncFiles;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.TEXT_HTML;
 
-@RestController
+@Controller
 @RequestMapping
 public class Oauth2Controller {
 
     @GetMapping(value = "/authorize")
-    public Mono<ResponseEntity<?>> handleAuthorize(AuthorizationRequest request) {
-        return Mono.just(ResponseEntity
-                .status(OK)
-                .contentType(TEXT_HTML)
-                .body("<p1>Hello from Cello server</p1>"));
+    public Mono<ResponseEntity<?>> handleAuthorize(AuthorizationRequest request) throws IOException {
+        Path path = new ClassPathResource("static/login-page.html").getFile().toPath();
+
+        return Mono.fromFuture(AsyncFiles.readAllBytes(path))
+                .map((bytes) -> ResponseEntity
+                        .status(OK)
+                        .contentType(TEXT_HTML)
+                        .body(new String(bytes))
+                );
+
     }
 }

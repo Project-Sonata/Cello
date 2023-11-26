e98b46f67757e9290789ab83b6e78d2b78752fb1
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-26 14:02:32 +0200

    Provide the login page from provided path

---
+++ src/main/java/com/odeyalo/sonata/cello/support/PathLoginPageProvider.java
@@ -0,0 +1,30 @@
+package com.odeyalo.sonata.cello.support;
+
+import com.odeyalo.sonata.cello.support.io.ReactiveFileReader;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Provide the login page from provided path
+ */
+public class PathLoginPageProvider implements LoginPageProvider {
+    private final String htmlPagePath;
+    private final ReactiveFileReader reactiveFileReader;
+
+    public PathLoginPageProvider(String htmlPagePath, ReactiveFileReader reactiveFileReader) {
+        Assert.state(Files.exists(Path.of(htmlPagePath)), "Invalid path to html file was provided");
+        this.reactiveFileReader = reactiveFileReader;
+        this.htmlPagePath = htmlPagePath;
+    }
+
+    @Override
+    @NotNull
+    public Flux<ByteBuffer> getLoginPage() {
+        return reactiveFileReader.readAllBytes(htmlPagePath);
+    }
+}

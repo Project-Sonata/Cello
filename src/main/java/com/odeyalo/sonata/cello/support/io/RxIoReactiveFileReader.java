8ea275bfd68bc405e4dece72f295c3d0ae760ce0
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-27 13:17:27 +0200

    Made it spring bean

--- src/main/java/com/odeyalo/sonata/cello/support/io/RxIoReactiveFileReader.java
+++ src/main/java/com/odeyalo/sonata/cello/support/io/RxIoReactiveFileReader.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.cello.support.io;
 
 import org.javaync.io.AsyncFiles;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -11,6 +12,7 @@ import java.nio.file.Path;
 /**
  * Uses RxIO library to read file reactively
  */
+@Component
 public class RxIoReactiveFileReader implements ReactiveFileReader {
     private static final int ONE_MEGABYTE = 1024 * 1024;
 

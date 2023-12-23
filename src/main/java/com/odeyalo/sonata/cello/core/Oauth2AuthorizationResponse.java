6e7989c475c238bcdd90bdb755c2e634adc8dbe8
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 15:05:55 +0200

    Added associated request to this response.

--- src/main/java/com/odeyalo/sonata/cello/core/Oauth2AuthorizationResponse.java
+++ src/main/java/com/odeyalo/sonata/cello/core/Oauth2AuthorizationResponse.java
@@ -1,8 +1,17 @@
 package com.odeyalo.sonata.cello.core;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represent any possible oauth2 successful authorization response that should be returned to the client
  */
-public interface Oauth2AuthorizationResponse {
+public interface Oauth2AuthorizationResponse<T extends Oauth2AuthorizationRequest> {
+    /**
+     * All responses must have associated request.
+     * Made it with generic to reduce casting complexity and make code more readable
+     * @return - associated request with this respose
+     */
+    @NotNull
+    T getAssociatedRequest();
 
 }

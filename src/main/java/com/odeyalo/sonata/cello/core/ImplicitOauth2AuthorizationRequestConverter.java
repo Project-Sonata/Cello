c41c990e7c0898bc659a2abdffe68d84a231ccd5
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 15:07:25 +0200

    Convert the ServerWebExchange to ImplicitOauth2AuthorizationRequest

---
+++ src/main/java/com/odeyalo/sonata/cello/core/ImplicitOauth2AuthorizationRequestConverter.java
@@ -0,0 +1,71 @@
+package com.odeyalo.sonata.cello.core;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Convert the {@link ServerWebExchange} to {@link ImplicitOauth2AuthorizationRequest}
+ */
+@Component
+public class ImplicitOauth2AuthorizationRequestConverter implements Oauth2AuthorizationRequestConverter {
+
+    public static final String SCOPES_SPLITERATOR = " ";
+
+    @Override
+    @NotNull
+    public Mono<Oauth2AuthorizationRequest> convert(@NotNull ServerWebExchange exchange) {
+        MultiValueMap<String, String> requestParameters = exchange.getRequest().getQueryParams();
+
+        Oauth2ResponseType responseType = Oauth2ResponseType.create(
+                requestParameters.getFirst(Oauth2RequestParameters.RESPONSE_TYPE)
+        );
+
+        if ( !Objects.equals(responseType, DefaultOauth2ResponseTypes.IMPLICIT) ) {
+            return Mono.empty();
+        }
+
+        var builder = ImplicitOauth2AuthorizationRequest.builder();
+
+        String clientId = requestParameters.getFirst(Oauth2RequestParameters.CLIENT_ID);
+
+        if ( clientId == null ) {
+            return Mono.error(
+                    MalformedOauth2RequestException.withCustomMessage("Missed client id parameter")
+            );
+        }
+
+        builder.clientId(clientId);
+
+        String state = requestParameters.getFirst(Oauth2RequestParameters.STATE);
+
+        if ( state != null ) {
+            builder.state(state);
+        }
+
+        String redirectUri = requestParameters.getFirst(Oauth2RequestParameters.REDIRECT_URI);
+
+        if ( redirectUri != null ) {
+            builder.redirectUri(RedirectUri.create(redirectUri));
+        }
+
+        String scopesStr = requestParameters.getFirst(Oauth2RequestParameters.SCOPE);
+
+        if ( scopesStr != null ) {
+            List<SimpleScope> scopes = Arrays.stream(scopesStr.split(SCOPES_SPLITERATOR))
+                    .map(SimpleScope::withName)
+                    .toList();
+
+            builder.scopes(ScopeContainer.fromCollection(scopes));
+        }
+        return Mono.just(
+                builder.build()
+        );
+    }
+}
---
+++ src/test/java/com/odeyalo/sonata/cello/core/ImplicitOauth2AuthorizationRequestConverterTest.java
@@ -0,0 +1,251 @@
+package com.odeyalo.sonata.cello.core;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.server.MockServerWebExchange;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
+
+class ImplicitOauth2AuthorizationRequestConverterTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"code", "custom_one"})
+    void shouldReturnEmptyOnNonTokenResponseType(String responseType) {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest.get("/hello")
+                        .queryParam("response_type", responseType)
+                        .queryParam("client_id", "123")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("state", "mikuiloveyou")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldNotReturnAnyExceptionIfRequestValid() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "123")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("state", "mikuiloveyou")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnSpecificAuthorizationRequest() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "123")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("state", "mikuiloveyou")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> request instanceof ImplicitOauth2AuthorizationRequest)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnClientIdEqualToProvided() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("state", "mikuiloveyou")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+                    return Objects.equals(implicitRequest.getClientId(), "mikuloverid");
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnStateEqualToProvided() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("state", "mikuiloveyou")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+                    return Objects.equals(implicitRequest.getState(), "mikuiloveyou");
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnNullStateIfNotProvided() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+                    return Objects.isNull(implicitRequest.getState());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnRedirectUriEqualToProvided() {
+        String redirectUri = "http://localhost:8080/oauth2/cello/callback";
+
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("redirect_uri", redirectUri)
+                        .queryParam("state", "mystate")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+                    return Objects.equals(implicitRequest.getRedirectUri().uriString(), redirectUri);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnNullRedirectUriIfNotProvided() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("state", "mystate")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+                    return Objects.isNull(implicitRequest.getRedirectUri());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnAllScopesProvidedInRequest() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("state", "mystate")
+                        .queryParam("scope", "write read delete")
+                        .build()
+        );
+
+        List<Scope> expectedScopes = List.of(
+                SimpleScope.withName("write"),
+                SimpleScope.withName("read"),
+                SimpleScope.withName("delete")
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+
+                    return implicitRequest.getScopes().containsAll(expectedScopes);
+
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnEmptyScopesIfScopeWasNotProvided() {
+        ImplicitOauth2AuthorizationRequestConverter testable = new ImplicitOauth2AuthorizationRequestConverter();
+
+        var exchange = MockServerWebExchange.from(
+                MockServerHttpRequest
+                        .get("/authorize")
+                        .queryParam("response_type", "token")
+                        .queryParam("client_id", "mikuloverid")
+                        .queryParam("state", "mystate")
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth2/cello/callback")
+                        .build()
+        );
+
+        testable.convert(exchange)
+                .as(StepVerifier::create)
+                .expectNextMatches(request -> {
+                    ImplicitOauth2AuthorizationRequest implicitRequest = (ImplicitOauth2AuthorizationRequest) request;
+
+                    return implicitRequest.getScopes().isEmpty();
+                })
+                .verifyComplete();
+    }
+}

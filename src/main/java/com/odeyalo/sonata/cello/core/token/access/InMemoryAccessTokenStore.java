bc828dcf5107071797a4f148e2d13182b3631e62
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 22:15:15 +0200

    Implementation of AccessTokenStore that store the AccessToken in ConcurrentMap

---
+++ src/main/java/com/odeyalo/sonata/cello/core/token/access/InMemoryAccessTokenStore.java
@@ -0,0 +1,35 @@
+package com.odeyalo.sonata.cello.core.token.access;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Implementation of {@link AccessTokenStore} that store the {@link AccessToken} in {@link ConcurrentMap}
+ */
+@Component
+public class InMemoryAccessTokenStore implements AccessTokenStore {
+    public final ConcurrentMap<String, AccessToken> tokens = new ConcurrentHashMap<>();
+
+    @Override
+    @NotNull
+    public Mono<Void> saveToken(@NotNull AccessToken accessToken) {
+        tokens.put(accessToken.getTokenValue(), accessToken);
+        return Mono.empty();
+    }
+
+    @Override
+    @NotNull
+    public Mono<AccessToken> findTokenByTokenValue(@NotNull String tokenValue) {
+        return Mono.justOrEmpty(tokens.get(tokenValue));
+    }
+
+    @Override
+    @NotNull
+    public Mono<Boolean> exists(@NotNull String tokenValue) {
+        return Mono.just(tokens.containsKey(tokenValue));
+    }
+}
---
+++ src/test/java/com/odeyalo/sonata/cello/core/token/access/InMemoryAccessTokenStoreTest.java
@@ -0,0 +1,104 @@
+package com.odeyalo.sonata.cello.core.token.access;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class InMemoryAccessTokenStoreTest {
+
+    @Test
+    void shouldCompletedSuccessfullyOnSaved() {
+        InMemoryAccessTokenStore testable = new InMemoryAccessTokenStore();
+
+        AccessToken token = AccessToken.builder()
+                .tokenValue("hello")
+                .issuedAt(Instant.now())
+                .expiresIn(Instant.now().plus(5, ChronoUnit.MINUTES))
+                .build();
+
+        testable.saveToken(token)
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldSaveTheToken() {
+        InMemoryAccessTokenStore testable = new InMemoryAccessTokenStore();
+
+        AccessToken token = AccessToken.builder()
+                .tokenValue("hello")
+                .issuedAt(Instant.now())
+                .expiresIn(Instant.now().plus(5, ChronoUnit.MINUTES))
+                .build();
+
+        testable.saveToken(token)
+                .as(StepVerifier::create)
+                .verifyComplete();
+
+        testable.findTokenByTokenValue(token.getTokenValue())
+                .as(StepVerifier::create)
+                .expectNext(token)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnNothingIfNotExistByTokenValue() {
+        InMemoryAccessTokenStore testable = new InMemoryAccessTokenStore();
+
+        AccessToken token = AccessToken.builder()
+                .tokenValue("hello")
+                .issuedAt(Instant.now())
+                .expiresIn(Instant.now().plus(5, ChronoUnit.MINUTES))
+                .build();
+
+        testable.saveToken(token)
+                .as(StepVerifier::create)
+                .verifyComplete();
+
+        testable.findTokenByTokenValue("not_exist")
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnTrueIfTokenExistByValue() {
+        InMemoryAccessTokenStore testable = new InMemoryAccessTokenStore();
+
+        AccessToken token = AccessToken.builder()
+                .tokenValue("hello")
+                .issuedAt(Instant.now())
+                .expiresIn(Instant.now().plus(5, ChronoUnit.MINUTES))
+                .build();
+
+        testable.saveToken(token)
+                .as(StepVerifier::create)
+                .verifyComplete();
+
+        testable.exists(token.getTokenValue())
+                .as(StepVerifier::create)
+                .expectNext(Boolean.TRUE)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnFalseIfNotExist() {
+        InMemoryAccessTokenStore testable = new InMemoryAccessTokenStore();
+
+        AccessToken token = AccessToken.builder()
+                .tokenValue("hello")
+                .issuedAt(Instant.now())
+                .expiresIn(Instant.now().plus(5, ChronoUnit.MINUTES))
+                .build();
+
+        testable.saveToken(token)
+                .as(StepVerifier::create)
+                .verifyComplete();
+
+        testable.exists("not_exist")
+                .as(StepVerifier::create)
+                .expectNext(Boolean.FALSE)
+                .verifyComplete();
+    }
+}

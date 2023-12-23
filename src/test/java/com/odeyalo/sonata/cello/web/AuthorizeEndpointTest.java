7fc616bc32f3c92cc48e782e5d9757ab25d51738
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 15:32:50 +0200

    Removed some tests

--- src/test/java/com/odeyalo/sonata/cello/web/AuthorizeEndpointTest.java
+++ src/test/java/com/odeyalo/sonata/cello/web/AuthorizeEndpointTest.java
@@ -8,17 +8,10 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 import static com.odeyalo.sonata.cello.core.Oauth2RequestParameters.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
@@ -77,64 +70,13 @@ class AuthorizeEndpointTest {
             exchange.expectStatus().isBadRequest();
         }
 
-        @Test
-        void shouldReturn400BadRequestIfRedirectUriNotIncluded() {
-            WebTestClient.ResponseSpec exchange = webTestClient.get()
-                    .uri(builder ->
-                            builder
-                                    .path("/authorize")
-                                    .queryParam(RESPONSE_TYPE, "token")
-                                    .queryParam(CLIENT_ID, "123")
-                                    .queryParam(SCOPE, "read write")
-                                    .queryParam(STATE, "opaque")
-                                    .build())
-                    .cookie(AUTHENTICATION_COOKIE_NAME, AUTHENTICATION_COOKIE_VALUE)
-                    .exchange();
-
-            exchange.expectStatus().isBadRequest();
-        }
-
-        @Test
-        void shouldReturn400BadRequestIfScopeNotIncluded() {
-            WebTestClient.ResponseSpec exchange = webTestClient.get()
-                    .uri(builder ->
-                            builder
-                                    .path("/authorize")
-                                    .queryParam(RESPONSE_TYPE, "token")
-                                    .queryParam(CLIENT_ID, "123")
-                                    .queryParam(REDIRECT_URI, "http://localhost:4000")
-                                    .queryParam(STATE, "opaque")
-                                    .build())
-                    .cookie(AUTHENTICATION_COOKIE_NAME, AUTHENTICATION_COOKIE_VALUE)
-                    .exchange();
-
-            exchange.expectStatus().isBadRequest();
-        }
-
-        @Test
-        void shouldReturn400BadRequestIfStateNotIncluded() {
-            WebTestClient.ResponseSpec exchange = webTestClient.get()
-                    .uri(builder ->
-                            builder
-                                    .path("/authorize")
-                                    .queryParam(RESPONSE_TYPE, "token")
-                                    .queryParam(CLIENT_ID, "123")
-                                    .queryParam(REDIRECT_URI, "http://localhost:4000")
-                                    .queryParam(SCOPE, "read write")
-                                    .build())
-                    .cookie(AUTHENTICATION_COOKIE_NAME, AUTHENTICATION_COOKIE_VALUE)
-                    .exchange();
-
-            exchange.expectStatus().isBadRequest();
-        }
-
         @NotNull
         private WebTestClient.ResponseSpec sendValidAuthorizeRequest() {
             return webTestClient.get()
                     .uri(builder ->
                             builder
                                     .path("/authorize")
-                                    .queryParam(RESPONSE_TYPE, "code")
+                                    .queryParam(RESPONSE_TYPE, "token")
                                     .queryParam(CLIENT_ID, "123")
                                     .queryParam(REDIRECT_URI, "http://localhost:4000")
                                     .queryParam(SCOPE, "read write")

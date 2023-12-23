c9eb93807fd99ccfdf4d0be6e25fd08c5f1ff5a4
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 15:03:51 +0200

    Now represent ANY oauth2 authorization request. Earlier only authorization code has been supported. Now it is interface and can be cast to any required implementation

---
+++ src/main/java/com/odeyalo/sonata/cello/core/Oauth2AuthorizationRequest.java
@@ -0,0 +1,10 @@
+package com.odeyalo.sonata.cello.core;
+
+/**
+ * Represent specific Oauth2 authorization request
+ *
+ * @see ImplicitOauth2AuthorizationRequest
+ */
+public interface Oauth2AuthorizationRequest {
+
+}

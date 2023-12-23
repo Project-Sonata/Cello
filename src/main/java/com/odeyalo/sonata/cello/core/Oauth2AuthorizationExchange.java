6937d21392073c4313e08e675c7901317f94ebfd
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-23 15:02:50 +0200

    Removed unused classes

--- src/main/java/com/odeyalo/sonata/cello/core/Oauth2AuthorizationExchange.java
+++
@@ -1,7 +0,0 @@
-package com.odeyalo.sonata.cello.core;
-
-import org.jetbrains.annotations.NotNull;
-
-public record Oauth2AuthorizationExchange(@NotNull AuthorizationRequest request,
-                                          @NotNull Oauth2AuthorizationResponse response) {
-}

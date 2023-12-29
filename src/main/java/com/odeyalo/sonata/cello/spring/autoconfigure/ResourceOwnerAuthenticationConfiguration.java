187eedcc0e1079330e4a9e4dcf28531970ba932e
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-29 15:12:19 +0200

    Removed unused imports

--- src/main/java/com/odeyalo/sonata/cello/spring/autoconfigure/ResourceOwnerAuthenticationConfiguration.java
+++ src/main/java/com/odeyalo/sonata/cello/spring/autoconfigure/ResourceOwnerAuthenticationConfiguration.java
@@ -2,20 +2,13 @@ package com.odeyalo.sonata.cello.spring.autoconfigure;
 
 import com.odeyalo.sonata.cello.core.authentication.AuthenticationPageProvider;
 import com.odeyalo.sonata.cello.core.authentication.DefaultAuthenticationPageProvider;
-import com.odeyalo.sonata.cello.core.authentication.StaticHtmlLoginPageRendererAuthenticationPageProvider;
 import com.odeyalo.sonata.cello.core.authentication.resourceowner.*;
-import com.odeyalo.sonata.cello.core.authentication.support.StaticLoginPageProperties;
-import com.odeyalo.sonata.cello.support.io.ReactiveFileReader;
-import com.odeyalo.sonata.cello.support.io.RxIoReactiveFileReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.server.savedrequest.ServerRequestCache;
-import org.springframework.util.ResourceUtils;
-
-import java.io.FileNotFoundException;
 
 @Configuration
 public class ResourceOwnerAuthenticationConfiguration {

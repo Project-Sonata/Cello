0cc25342f2648b6cdfbd9aef3ac3c2af84fec041
odeyalo <theprogrammerinfuture@gmail.com>
2023-12-31 21:08:38 +0200

    Handle the denied submission of oauth2 consent by the resource owner.

---
+++ src/main/java/com/odeyalo/sonata/cello/core/consent/Oauth2ConsentDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.odeyalo.sonata.cello.core.consent;
+
+import com.odeyalo.sonata.cello.core.Oauth2AuthorizationRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Handle the denied submission of oauth2 consent by the resource owner.
+ */
+public interface Oauth2ConsentDeniedHandler {
+    /**
+     * Invoked when resource owner denied the oauth2 consent.
+     * @param request - current authorization request
+     * @param decision - decision made by the user
+     * @param httpExchange - current http exchange
+     * @return - empty {@link Mono}, response is written in {@link ServerWebExchange#getResponse()}
+     */
+    @NotNull
+    Mono<Void> onConsentDenied(@NotNull Oauth2AuthorizationRequest request,
+                               @NotNull ConsentDecision decision,
+                               @NotNull ServerWebExchange httpExchange);
+}

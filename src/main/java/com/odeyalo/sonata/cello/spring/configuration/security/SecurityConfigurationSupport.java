1b8620fe45171779ada09dc5d61f5f94b900c60a
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-08 18:43:12 +0200

    Changed RedirectServerAuthenticationEntryPoint to Oauth2FlowIdAwareRedirectServerAuthenticationEntryPoint

--- src/main/java/com/odeyalo/sonata/cello/spring/configuration/security/SecurityConfigurationSupport.java
+++ src/main/java/com/odeyalo/sonata/cello/spring/configuration/security/SecurityConfigurationSupport.java
@@ -2,9 +2,14 @@ package com.odeyalo.sonata.cello.spring.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.server.DefaultServerRedirectStrategy;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
-import org.springframework.security.web.server.savedrequest.ServerRequestCache;
+import org.springframework.security.web.server.ServerRedirectStrategy;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 /**
  * Support configuration to create bean that can be created in {@link com.odeyalo.sonata.cello.spring.configuration.SecurityConfiguration} because they are in creation
@@ -13,9 +18,23 @@ import org.springframework.security.web.server.savedrequest.ServerRequestCache;
 public class SecurityConfigurationSupport {
 
     @Bean
-    public ServerAuthenticationEntryPoint serverAuthenticationEntryPoint(ServerRequestCache cache) {
-        RedirectServerAuthenticationEntryPoint entryPoint = new RedirectServerAuthenticationEntryPoint("/login");
-        entryPoint.setRequestCache(cache);
-        return entryPoint;
+    public ServerAuthenticationEntryPoint serverAuthenticationEntryPoint() {
+        return new Oauth2FlowIdAwareRedirectServerAuthenticationEntryPoint();
+    }
+
+    /**
+     * Redirect the resource owner to /login endpoint with 'flow_id' parameter
+     */
+    private static class Oauth2FlowIdAwareRedirectServerAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+        private final ServerRedirectStrategy serverRedirectStrategy = new DefaultServerRedirectStrategy();
+
+        @Override
+        public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException ex) {
+            String flowId = (String) exchange.getAttributes().get("flow_id");
+            if ( flowId == null ) {
+                return Mono.error(new IllegalStateException("Flow id is required by standard!"));
+            }
+            return serverRedirectStrategy.sendRedirect(exchange, URI.create("/login?flow_id=" + flowId));
+        }
     }
 }

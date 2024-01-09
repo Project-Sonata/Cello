7324f4be22e1498d5476b269e72caf052f328d65
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-09 21:28:44 +0200

    Utils for URIs

---
+++ src/test/java/testing/UriUtils.java
@@ -0,0 +1,21 @@
+package testing;
+
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UriUtils {
+
+    public static Map<String, String> parseQueryParameters(URI url) {
+        Map<String, String> queryPairs = new HashMap<>();
+        String query = url.getQuery();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            queryPairs.put(URLDecoder.decode(pair.substring(0, idx), StandardCharsets.UTF_8), URLDecoder.decode(pair.substring(idx + 1), StandardCharsets.UTF_8));
+        }
+        return queryPairs;
+    }
+}

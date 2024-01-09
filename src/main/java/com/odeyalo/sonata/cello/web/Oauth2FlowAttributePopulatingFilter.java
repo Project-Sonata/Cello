f2ba47ab09acd4bf24927928252106eec42838f2
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-09 19:05:17 +0200

    Added support for POST method

--- src/main/java/com/odeyalo/sonata/cello/web/Oauth2FlowAttributePopulatingFilter.java
+++ src/main/java/com/odeyalo/sonata/cello/web/Oauth2FlowAttributePopulatingFilter.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.cello.web;
 
 import com.odeyalo.sonata.cello.exception.MissingAuthorizationRequestFlowIdException;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.RequestPath;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -32,6 +33,14 @@ public class Oauth2FlowAttributePopulatingFilter implements WebFilter {
         }
 
         String flowId = exchange.getRequest().getQueryParams().getFirst("flow_id");
+
+        if (exchange.getRequest().getMethod() == HttpMethod.POST) {
+            return exchange.getFormData()
+                    .mapNotNull(formData -> formData.getFirst("flow_id"))
+                    .switchIfEmpty(Mono.error(MissingAuthorizationRequestFlowIdException.withCustomMessage("Missing 'flow_id' parameter!")))
+                    .doOnNext(flow -> exchange.getAttributes().put(CURRENT_FLOW_ATTRIBUTE_NAME, flow))
+                    .then(chain.filter(exchange));
+        }
 
         if ( flowId == null ) {
             return Mono.error(MissingAuthorizationRequestFlowIdException.withCustomMessage("Missing 'flow_id' parameter!"));

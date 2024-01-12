9196043b6900a00e129c93aeb973fcef93387c92
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-12 18:14:27 +0200

    @ConditionalOnProperty(value = "cello.enabled", havingValue = "true") Added

--- src/main/java/com/odeyalo/sonata/cello/spring/autoconfigure/CelloServerAutoConfiguration.java
+++ src/main/java/com/odeyalo/sonata/cello/spring/autoconfigure/CelloServerAutoConfiguration.java
@@ -1,8 +1,10 @@
 package com.odeyalo.sonata.cello.spring.autoconfigure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.odeyalo.sonata.cello")
+@ConditionalOnProperty(value = "cello.enabled", havingValue = "true")
 public class CelloServerAutoConfiguration {
 
 }

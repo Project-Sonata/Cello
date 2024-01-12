541b60bbed9a9a92acdb5fb8def092d3ede09f51
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-12 13:48:57 +0200

    Small refactoring, renamed methods and removed toUriWithoutQueryParams, now using UriAssert.isEqualToWithoutQueryParameters

--- src/test/java/com/odeyalo/sonata/cello/core/consent/DelegatingOauth2ConsentSubmissionHandlerTest.java
+++ src/test/java/com/odeyalo/sonata/cello/core/consent/DelegatingOauth2ConsentSubmissionHandlerTest.java
@@ -6,7 +6,6 @@ import com.odeyalo.sonata.cello.core.RedirectUri;
 import com.odeyalo.sonata.cello.core.RedirectUriProvider;
 import com.odeyalo.sonata.cello.core.authentication.resourceowner.ResourceOwner;
 import lombok.Value;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,16 +13,16 @@ import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.security.web.server.savedrequest.CookieServerRequestCache;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import testing.UriAssert;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DelegatingOauth2ConsentSubmissionHandlerTest {
 
     @Test
-    void shouldReturnRedirectToAuthorizeOnSuccess() {
+    void shouldReturnRedirectToSavedRequestOnApproved() {
         // given
         CookieServerRequestCache serverRequestCache = new CookieServerRequestCache();
         serverRequestCache.setSaveRequestMatcher(ServerWebExchangeMatchers.anyExchange());
@@ -40,21 +39,21 @@ class DelegatingOauth2ConsentSubmissionHandlerTest {
                 new DefaultOauth2ConsentDeniedHandler()
         );
 
-        MockServerWebExchange webExchange = MockServerWebExchange.from(
+        MockServerWebExchange approvedConsentExchange = MockServerWebExchange.from(
                 MockServerHttpRequest.post("/oauth2/consent")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .cookie(initialExchange.getResponse().getCookies().getFirst("REDIRECT_URI"))
                         .body("action=approved")
         );
         // when
-        testable.handleConsentSubmission(MockOauth2AuthorizationRequest.create(), ResourceOwner.withPrincipalOnly("odeyalo"), webExchange).block();
+        testable.handleConsentSubmission(MockOauth2AuthorizationRequest.create(), ResourceOwner.withPrincipalOnly("odeyalo"), approvedConsentExchange).block();
         // then
-        assertThat(webExchange.getResponse().getStatusCode()).isEqualTo(HttpStatus.FOUND);
-        assertThat(webExchange.getResponse().getHeaders().getLocation()).isEqualTo(initialExchange.getRequest().getURI());
+        assertThat(approvedConsentExchange.getResponse().getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        assertThat(approvedConsentExchange.getResponse().getHeaders().getLocation()).isEqualTo(initialExchange.getRequest().getURI());
     }
 
     @Test
-    void shouldReturnRedirectWithErrorOnFailed() throws URISyntaxException {
+    void shouldReturnRedirectWithErrorOnDenied() {
         // given
         CookieServerRequestCache serverRequestCache = new CookieServerRequestCache();
         serverRequestCache.setSaveRequestMatcher(ServerWebExchangeMatchers.anyExchange());
@@ -78,28 +77,20 @@ class DelegatingOauth2ConsentSubmissionHandlerTest {
                         .body("action=denied")
         );
         // when
-        testable.handleConsentSubmission(new RedirectUriRequest(RedirectUri.create("http://localhost:3000/callback")), ResourceOwner.withPrincipalOnly("odeyalo"), webExchange).block();
+        testable.handleConsentSubmission(new RedirectUriRequest(RedirectUri.create("http://localhost:3000/callback")),
+                ResourceOwner.withPrincipalOnly("odeyalo"),
+                webExchange)
+                .block();
+
         // then
         assertThat(webExchange.getResponse().getStatusCode()).isEqualTo(HttpStatus.FOUND);
 
         URI redirectLocation = webExchange.getResponse().getHeaders().getLocation();
 
-        assertThat(redirectLocation).isNotNull();
-
-        assertThat(toUriWithoutQueryParams(redirectLocation)).isEqualTo(URI.create("http://localhost:3000/callback"));
+        UriAssert.assertThat(redirectLocation).isEqualToWithoutQueryParameters("http://localhost:3000/callback");
 
         assertThat(redirectLocation).hasParameter("error", "access_denied");
     }
-
-    @NotNull
-    private static URI toUriWithoutQueryParams(URI redirectLocation) throws URISyntaxException {
-        return new URI(redirectLocation.getScheme(),
-                redirectLocation.getAuthority(),
-                redirectLocation.getPath(),
-                null, // Ignore the query part of the input url
-                redirectLocation.getFragment());
-    }
-
 
     @Value
     private static class RedirectUriRequest implements Oauth2AuthorizationRequest, RedirectUriProvider {

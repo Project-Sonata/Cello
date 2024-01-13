2fdff877bd57a27d538bc1486d6a6b5dbf523af5
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-13 20:02:27 +0200

    Contain specification of names for endpoints that used by Cello

---
+++ src/main/java/com/odeyalo/sonata/cello/web/CelloOauth2ServerEndpointsSpec.java
@@ -0,0 +1,76 @@
+package com.odeyalo.sonata.cello.web;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Contain specification of names for endpoints that used by Cello
+ */
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+public final class CelloOauth2ServerEndpointsSpec {
+    @NotNull
+    @Builder.Default
+    String prefix = "/oauth2";
+    @NotNull
+    @Builder.Default
+    String authorizeEndpoint = "/authorize";
+    @NotNull
+    @Builder.Default
+    String loginEndpoint = "/login";
+    @NotNull
+    @Builder.Default
+    String consentEndpoint = "/consent";
+
+    public static CelloOauth2ServerEndpointsSpec defaultSpec() {
+        return builder()
+                .prefix("/oauth2")
+                .consentEndpoint("/consent")
+                .loginEndpoint("/login")
+                .authorizeEndpoint("/authorize")
+                .build();
+    }
+
+    public CelloOauth2ServerEndpointsSpec(@NotNull String prefix, @NotNull String authorizeEndpoint, @NotNull String loginEndpoint, @NotNull String consentEndpoint) {
+        setPrefix(prefix);
+        setAuthorizeEndpoint(authorizeEndpoint);
+        setLoginEndpoint(loginEndpoint);
+        setConsentEndpoint(consentEndpoint);
+    }
+
+    private void setPrefix(@NotNull String prefix) {
+        if ( !StringUtils.endsWith(prefix, "/") ) {
+            prefix += "/";
+        }
+        this.prefix = prefix;
+    }
+
+    private void setAuthorizeEndpoint(@NotNull String authorizeEndpoint) {
+        authorizeEndpoint = maybeStripSlash(authorizeEndpoint);
+        this.authorizeEndpoint = prefix + authorizeEndpoint;
+    }
+
+
+    private void setLoginEndpoint(@NotNull String loginEndpoint) {
+        loginEndpoint = maybeStripSlash(loginEndpoint);
+        this.loginEndpoint = loginEndpoint;
+    }
+
+    private void setConsentEndpoint(@NotNull String consentEndpoint) {
+        consentEndpoint = maybeStripSlash(consentEndpoint);
+        this.consentEndpoint = consentEndpoint;
+    }
+
+    @NotNull
+    private static String maybeStripSlash(@NotNull String uri) {
+        if ( StringUtils.startsWith(uri, "/") ) {
+            return uri.substring(1);
+        }
+        return uri;
+    }
+}

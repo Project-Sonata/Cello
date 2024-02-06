614e5f5cb0e2f4d274817816a08967371d858664
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-06 18:51:57 +0200

    Added docs, changed it to interface

--- src/main/java/com/odeyalo/sonata/cello/core/authentication/oauth2/Oauth2ProviderRedirectUriProviderManager.java
+++ src/main/java/com/odeyalo/sonata/cello/core/authentication/oauth2/Oauth2ProviderRedirectUriProviderManager.java
@@ -1,39 +1,20 @@
 package com.odeyalo.sonata.cello.core.authentication.oauth2;
 
-import com.odeyalo.sonata.cello.core.Oauth2RequestParameters;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
-import java.util.Map;
-import java.util.UUID;
 
-@Component
-public final class Oauth2ProviderRedirectUriProviderManager {
-    private final Map<String, Oauth2ProviderRegistration> providers;
-
-    public Oauth2ProviderRedirectUriProviderManager(Map<String, Oauth2ProviderRegistration> providers) {
-        this.providers = providers;
-    }
-
+/**
+ * Provide the URI to redirect the resource owner to authenticate the RO using Oauth2 Provider
+ */
+public interface Oauth2ProviderRedirectUriProviderManager {
+    /**
+     * Build ready-to-use redirect uri for the given provider, the resource owner will be redirected to this uri to enter its credentials
+     *
+     * @param providerName - provider name the resource owner wants to use
+     * @return - {@link Mono} with {@link URI} to redirect RO to, empty {@link Mono} if not supported
+     */
     @NotNull
-    public Mono<URI> getProviderRedirectUri(@NotNull String providerName) {
-        if ( !providers.containsKey(providerName) ) {
-            return Mono.empty();
-        }
-
-        return Mono.fromCallable(() -> {
-            Oauth2ProviderRegistration oauth2ProviderRegistration = providers.get(providerName);
-
-            return UriComponentsBuilder.fromUriString(oauth2ProviderRegistration.getProviderUri())
-                    .queryParam(Oauth2RequestParameters.STATE, UUID.randomUUID().toString())
-                    .queryParam(Oauth2RequestParameters.RESPONSE_TYPE, "code")
-                    .queryParam(Oauth2RequestParameters.REDIRECT_URI, oauth2ProviderRegistration.getRedirectUri())
-                    .queryParam(Oauth2RequestParameters.CLIENT_ID, oauth2ProviderRegistration.getClientId())
-                    .queryParam(Oauth2RequestParameters.SCOPE, String.join(" ", oauth2ProviderRegistration.getScopes()))
-                    .build().toUri();
-        });
-    }
+    Mono<URI> getProviderRedirectUri(@NotNull String providerName);
 }

54903d89c7267a8e1dca8966bed4d2e2037bee1b
odeyalo <theprogrammerinfuture@gmail.com>
2024-04-17 18:43:51 +0300

    added component annotation for UUIDOauth2StateGenerator

--- src/main/java/com/odeyalo/sonata/cello/core/authentication/oauth2/UUIDOauth2StateGenerator.java
+++ src/main/java/com/odeyalo/sonata/cello/core/authentication/oauth2/UUIDOauth2StateGenerator.java
@@ -1,6 +1,7 @@
 package com.odeyalo.sonata.cello.core.authentication.oauth2;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.UUID;
 /**
  * An implementation of {@link Oauth2StateGenerator} that just generates a UUID strings
  */
+@Component
 public final class UUIDOauth2StateGenerator implements Oauth2StateGenerator {
 
     @Override

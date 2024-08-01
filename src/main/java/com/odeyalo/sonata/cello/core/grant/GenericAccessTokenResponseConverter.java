8a09d7d506c88a0be1b51139b49ccc8c1b679c00
odeyalo <theprogrammerinfuture@gmail.com>
2024-08-01 15:21:00 +0300

    Fixed typo in GenericAccessTokenResponseConverter

--- src/main/java/com/odeyalo/sonata/cello/core/grant/GenericAccessTokenResponseConverter.java
+++ src/main/java/com/odeyalo/sonata/cello/core/grant/GenericAccessTokenResponseConverter.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * Generic {@link AccessTokenRequestConverter} that converts any {@link AccessTokenResponse}
+ * Generic {@link AccessTokenResponseConverter} that converts any {@link AccessTokenResponse}
  */
 public final class GenericAccessTokenResponseConverter implements AccessTokenResponseConverter {
     private final ResponseWriter accessTokenResponseWriter;

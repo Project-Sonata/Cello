772f68a9a44ce6d32b35060101bb18f960d582a0
odeyalo <theprogrammerinfuture@gmail.com>
2024-08-01 15:25:23 +0300

    Replaced the field injection with constructor based

--- src/main/java/com/odeyalo/sonata/cello/web/TokenController.java
+++ src/main/java/com/odeyalo/sonata/cello/web/TokenController.java
@@ -2,11 +2,8 @@ package com.odeyalo.sonata.cello.web;
 
 import com.odeyalo.sonata.cello.core.grant.AccessTokenRequestConverter;
 import com.odeyalo.sonata.cello.core.grant.AccessTokenResponseConverter;
-import com.odeyalo.sonata.cello.core.grant.AuthorizationCodeAccessTokenRequest;
 import com.odeyalo.sonata.cello.core.grant.GrantHandler;
-import com.odeyalo.sonata.cello.core.responsetype.code.support.AuthorizationCodeService;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,24 +13,22 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/token")
 public final class TokenController {
+    private final AccessTokenRequestConverter accessTokenRequestConverter;
+    private final GrantHandler grantHandler;
+    private final AccessTokenResponseConverter accessTokenResponseConverter;
 
-    @Autowired
-    AccessTokenRequestConverter accessTokenRequestConverter;
-
-    @Autowired
-    AuthorizationCodeService authorizationCodeService;
-
-    @Autowired
-    GrantHandler grantHandler;
-
-    @Autowired
-    AccessTokenResponseConverter accessTokenResponseConverter;
+    public TokenController(final AccessTokenRequestConverter accessTokenRequestConverter,
+                           final GrantHandler grantHandler,
+                           final AccessTokenResponseConverter accessTokenResponseConverter) {
+        this.accessTokenRequestConverter = accessTokenRequestConverter;
+        this.grantHandler = grantHandler;
+        this.accessTokenResponseConverter = accessTokenResponseConverter;
+    }
 
     @PostMapping
     public Mono<Void> handlerAccessTokenRequest(@NotNull final ServerWebExchange exchange) {
         return accessTokenRequestConverter.convert(exchange)
-                .cast(AuthorizationCodeAccessTokenRequest.class)
-                .flatMap(request -> grantHandler.handle(request))
+                .flatMap(grantHandler::handle)
                 .flatMap(it -> accessTokenResponseConverter.writeResponse(it, exchange));
     }
 }
